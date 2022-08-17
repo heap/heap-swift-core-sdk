6b087ed5e2a4431cfd0526968f011b12d2114abe
Brian Nickel <brian.nickel@gmail.com>
2022-08-17 15:25:36 -0700

    [HEAP-34076] Add protobufs to the library (#3)

--- Package.swift
+++ Package.swift
@@ -16,14 +16,18 @@ let package = Package(
         .library(
             name: "HeapSwiftCore",
             targets: ["HeapSwiftCore"]),
-        
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
     ],
     targets: [
-        .target(name: "HeapSwiftCore"),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "HeapSwiftCore",
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+            ]),
         .testTarget(
             name: "HeapSwiftCoreTests",
             dependencies: ["HeapSwiftCore"]),
