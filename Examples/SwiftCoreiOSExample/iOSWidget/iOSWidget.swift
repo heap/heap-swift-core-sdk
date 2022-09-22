b58128eaf9eec9730dd48ba1d58b0dd4330c7b54
Bryan Mitchell <97151843+BryanMitchell-heap@users.noreply.github.com>
2022-09-22 16:16:28 -0700

    [HEAP-33674] iOS extension widget (#27)
    * Add iOS Widget to iOS Example Project

---
+++ Examples/SwiftCoreiOSExample/iOSWidget/iOSWidget.swift
@@ -0,0 +1,66 @@
+//
+//  iOSWidget.swift
+//  iOSWidget
+//
+//  Created by Bryan Mitchell on 9/22/22.
+//
+
+import WidgetKit
+import SwiftUI
+import HeapSwiftCore
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+        entries.append(SimpleEntry(date: Date()))
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct iOSWidgetEntryView : View {
+    var entry: Provider.Entry
+    
+    func rendered() {
+        print("Heap Widget Rendered")
+        Heap.shared.startRecording("11")
+        Heap.shared.track("Widget Rendered")
+    }
+    
+    var body: some View {
+        Text(entry.date, style: .time).onAppear { self.rendered() }
+    }
+}
+
+@main
+struct iOSWidget: Widget {
+    let kind: String = "iOSWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            iOSWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct iOSWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        iOSWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
