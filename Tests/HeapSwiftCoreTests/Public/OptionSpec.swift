c98cfb89eaf072265d173ed69e5dae688fd25f28
Brian Nickel <brian.nickel@heap.io>
2022-11-21 11:00:51 -0700

    Fix test tests (#49)

--- Tests/HeapSwiftCoreTests/Public/OptionSpec.swift
+++ Tests/HeapSwiftCoreTests/Public/OptionSpec.swift
@@ -3,6 +3,7 @@ import Quick
 import Nimble
 import XCTest
 @testable import HeapSwiftCore
+@testable import HeapSwiftCoreTestSupport
 
 extension Option {
     static let something = register(name: "something", type: .string)
