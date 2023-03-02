7433b437b611b4c03cd93744af1f881ea8d0e015
Brian Nickel <brian.nickel@heap.io>
2023-03-02 09:35:28 -0700

    [HEAP-40879] Bump version to 0.0.2 (#73)

--- Development/Sources/HeapSwiftCore/Version.swift
+++ Development/Sources/HeapSwiftCore/Version.swift
@@ -8,7 +8,7 @@ struct Version {
     static let minor = 0
 
     /// Revision number.
-    static let revision = 1
+    static let revision = 2
 
     /// Optional pre-release version
     static let prerelease: String? = nil
