2332bf3dd404b99b265e92342613fd917eb0303e
Brian Nickel <brian.nickel@heap.io>
2024-05-02 14:21:44 -0700

    [PA-4787] Expose more complex properties for CS session exchange (#155)

--- HeapSwiftCoreInterfaces/HeapSwiftCoreInterfaces/_ContentsquareIntegration.swift
+++ HeapSwiftCoreInterfaces/HeapSwiftCoreInterfaces/_ContentsquareIntegration.swift
@@ -1,14 +1,34 @@
 import Foundation
 
-public struct _ContentsquareSessionProperties {
-    public var createdByContentsquareScreenView: Bool = false
-    public var previousSessionHadDifferentUser: Bool = false
+public struct _ContentsquareSessionProperties: OptionSet {
+    public let rawValue: Int
     
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+    
+    public static let createdByContentsquare = _ContentsquareSessionProperties(rawValue: 1 << 0)
+    public static let createdByContentsquareScreenView = _ContentsquareSessionProperties(rawValue: (1 << 0) | (1 << 1)) // Implicitly created by CS.
+    public static let previousSessionHadDifferentUser = _ContentsquareSessionProperties(rawValue: 1 << 2)
+}
+
+public enum _ContentsquareSessionExtensionSource {
+    case screenview
+    case appStartOrShow
+    case other
+}
+
+public struct _AdvanceOrExtendSessionResults {
+    public var environmentId: String?
+    public var userId: String?
+    public var sessionId: String?
+    
+    public var newSessionCreated: Bool = false
     public init() {}
 }
 
 public protocol _ContentsquareMethods: AnyObject {
-    func advanceOrExtendSession(fromContentsquareScreenView: Bool) -> (environmentId: String, userId: String, sessionId: String)?
+    func advanceOrExtendSession(source: _ContentsquareSessionExtensionSource) -> _AdvanceOrExtendSessionResults
     var currentSessionProperties: _ContentsquareSessionProperties { get }
 }
 
